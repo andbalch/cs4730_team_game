4abfb19260a0fe4f0f19b49dd612e37d21e88b63
andbalch <andbalch@gmail.com>
2025-04-20 11:56:08 -0400

    expand simulation to allow for basic recipe-making

--- simulation.lua
+++ simulation.lua
@@ -43,23 +43,34 @@ function create_sim(x,y,w,h)
 		perm=function(s,x1,y1,x2,y2)
 			return density[s:g(x1,y1)+1]>density[s:g(x2,y2)+1]
 		end,
-		-- Try to move cell (x1,y1) into cell (x2,y2)
+		-- Try to move cell (x1,y1) into cell (x2,y2), checking for valid recipe/reaction
 		try=function(s,x1,y1,x2,y2)
 			local c1,c2=s:g(x1,y1),s:g(x2,y2)	
 
 			if c2~=13 and c1~=c2 then
-				-- Acid destroys.
-				if (c1==11 or c2==11) and c1~=0 and c2~=0 and c1~=6 and c2~=6 then
-					s:s(x1,y1,6)
-					s:s(x2,y2,6)
-					return true
-				elseif density[c1+1]>density[c2+1] then -- Check for permeability, and swap if it works.
+				adj_occupied = c1~=0 and c2~=0 and c1~=6 and c2~=6
+				if c1~=0 and c2~=0 and c1~=6 and c2~=6 then
+					-- Acid destroys.
+					if (c1==11 or c2==11) then
+						s:s(x1,y1,6)
+						s:s(x2,y2,6)
+						return true
+					-- Check if c1 and c2 make a recipe or reaction
+					else
+						prod = check_recipe(c1, c2)
+						if prod ~= -1 then 
+							s:s(x1,y1,prod)
+							s:s(x2,y2,prod)
+							return true
+						end
+					end
+				end
+				if density[c1+1]>density[c2+1] then -- Check for permeability, and swap if it works.
 					s:s(x1,y1,c2)
 					s:s(x2,y2,c1)
 					return true
 				end
 			end
-
 			return false
 		end
 	}
@@ -69,7 +80,7 @@ function update_sim(s)
 	s.t=s.t+1
 	local r=flr(rnd(10)) 
 	for x=0,s.w-1 do
-		-- Materials that move down.
+		-- Materials that move down. Do you mean "move up"? - Andrew
 		local sim_steam=s.t%4==0
 		for y=0,s.h-1 do
 			local c=s:g(x,y)
@@ -117,4 +128,53 @@ function draw_sim(s,dx,dy)
 		end
 	end
 	sspr(s.x,s.y,s.w,s.h,dx,dy,s.w,s.h)
+end
+
+-- Helper function to compare adjacent cell colors and see if they make a potion/reaction
+function check_recipe(c1, c2)
+	-- wyrmwood oil
+	if cmp_cells(c1, c2, 12, 14) then
+		return 15
+	-- caustic dreams
+	elseif cmp_cells(c1, c2, 12, 15) then
+		return 7
+	-- fortified runes
+	elseif cmp_cells(c1, c2, 15, 14) then
+		return 4
+	-- gaseous materia
+	elseif cmp_cells(c1, c2, 12, 4) then
+		return 5
+	-- dragon's blood
+	elseif cmp_cells(c1, c2, 7, 14) then
+		return 8
+	-- spesi cola
+	elseif cmp_cells(c1, c2, 12, 7) then
+		return 2
+	-- sweat of newt
+	elseif cmp_cells(c1, c2, 15, 5) then
+		return 3
+	-- dew of miasma
+	elseif cmp_cells(c1, c2, 7, 5) then
+		return 6
+	-- fenwick tree
+	elseif cmp_cells(c1, c2, 2, 4) then
+		return 9
+	-- holy tears
+	elseif cmp_cells(c1, c2, 3, 4) then
+		return 1
+	-- liquid algorithms
+	elseif cmp_cells(c1, c2, 9, 1) then
+		return 10
+
+	-- TODO: expand to include dangerous reactions. Can indicate these with a global or by returning a val > 15?
+
+	-- invalid recipe -> do nothing
+	else
+		return -1
+	end
+end
+
+-- Helper code to check if some combination of c1 and c2 are equal to v1 and v2
+function cmp_cells(c1, c2, v1, v2)
+	return (c1 == v1 and c2 == v2) or (c1 == v2 and c2 == v1)
 end
