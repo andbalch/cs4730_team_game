8a22c40f4895788ac44a38c7bafec0e4f6a9a43b
edruga <jhe6rv@virginia.edu>
2025-04-21 01:37:29 -0400

    Populate recipe book

--- recipes.lua
+++ recipes.lua
@@ -2,13 +2,19 @@
 -- TODO: handle recipes where the result is an effect rather than a potion
 -- TODO: populate with more recipes
 -- TODO: possibly multiple pages
+-- TODO: handle "discovering" recipes - simulation dependent so maybe someone else does this
 
 
 function init_recipes()
 
     recipe_book = {}
-    recipe_book[1] = create_recipe(14, 15, 4, false)
-    recipe_book[2] = create_recipe(12, 4, 5, false)
+    recipe_book[1] = create_recipe(12, 15, 7, true)
+    recipe_book[2] = create_recipe(14, 15, 4, true)
+    recipe_book[3] = create_recipe(12, 4, 5, false)
+    recipe_book[4] = create_recipe(14, 7, 8, false)
+    recipe_book[5] = create_recipe(4, 7, 2, false)
+    recipe_book[6] = create_recipe(15, 5, 3, false)
+    recipe_book[7] = create_recipe(7, 5, 6, false)
 
 end
 
@@ -26,29 +32,29 @@ end
 function write_recipe(rp, index)
 
     -- Write ingredient 1.
-    rect(30, 15, 36, 21, 13)
-    rectfill(31, 16, 35, 20, rp.ing_1)
+    rect(30, 15+(index*14), 36, 21+(index*14), 13)
+    rectfill(31, 16+(index*14), 35, 20+(index*14), rp.ing_1)
     
     -- Write ingredient 2.
-    rect(50, 15, 56, 21, 13)
-    rectfill(51, 16, 55, 20, rp.ing_2)
+    rect(50, 15+(index*14), 56, 21+(index*14), 13)
+    rectfill(51, 16+(index*14), 55, 20+(index*14), rp.ing_2)
 
     -- Write result, or question mark if recipe is not yet discovered.
     if rp.discovered then
-        rect(80, 15, 86, 21, 13)
-        rectfill(81, 16, 85, 20, rp.result)
+        rect(80, 15+(index*14), 86, 21+(index*14), 13)
+        rectfill(81, 16+(index*14), 85, 20+(index*14), rp.result)
     else
-        print("?", 81, 16, 13)
+        print("?", 81, 16+(index*14), 13)
     end
     
     -- Plus sign.
-    rectfill(43, 16, 43, 20, 13)
-    rectfill(41, 18, 45, 18, 13)
+    rectfill(43, 16+(index*14), 43, 20+(index*14), 13)
+    rectfill(41, 18+(index*14), 45, 18+(index*14), 13)
 
 
     -- Equal sign.
-    rectfill(66, 17, 70, 17, 13)
-    rectfill(66, 19, 70, 19, 13)
+    rectfill(66, 17+(index*14), 70, 17+(index*14), 13)
+    rectfill(66, 19+(index*14), 70, 19+(index*14), 13)
 
 end
 
@@ -107,7 +113,7 @@ function recipes_draw()
 
     -- Write recipes on page.
     for i = 1,#recipe_book do
-        write_recipe(recipe_book[i], i)
+        write_recipe(recipe_book[i], i-1)
     end
 
     -- Draw back button.
