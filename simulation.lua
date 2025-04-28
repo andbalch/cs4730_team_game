533f66b3bb6b54ddf2c92b2a0fbb31575f194aa3
Varun Varma <varranvar@gmail.com>
2025-04-28 18:34:49 -0400

    Prevents spores from moving out of vial

--- simulation.lua
+++ simulation.lua
@@ -146,7 +146,8 @@ function update_sim(s)
 						if c==10 then
 							if s.r > 7 then
 								local dx=-1+flr(rnd(3))
-								local dy=1+(-1*flr(rnd(2)))
+								local dy=-1+flr(rnd(3))
+								if y+dy<0 then dy=-dy end
 								local m=s:perm(x,y,x,y+dy) and s:try(x,y,x+dx,y+dy)
 							end
 						-- Random jumping for miasma.
